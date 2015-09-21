4aaed973aee9aee7cdfa8a64e176991cce2d521d
Alexey Saenko <qwelidus@gmail.com>
2015-09-21 16:24:54 +0300

    spring mvc is completely set

--- src/main/java/com/datapine/web/controller/HomeController.java
+++ src/main/java/com/datapine/web/controller/HomeController.java
@@ -7,11 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    /*
-     * This controller will only work, if the Spring MVC settings are set
-     * completely and correctly (just as a hint).
-     */
-
     @RequestMapping("/")
     public ModelAndView showWelcome() {
         return new ModelAndView("index");
