7be94a448fec29958799f393d10e9f5faa24b587
Alexey Saenko <qwelidus@gmail.com>
2015-09-23 15:43:53 +0300

    fixed qulice violations

--- src/main/java/com/datapine/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/datapine/service/impl/UserDetailsServiceImpl.java
@@ -51,8 +51,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private transient UserDAO dao;
 
-    private final List<User> admins = Collections.unmodifiableList(
+    /**
+     * Unmodifiable list of admin users.
+     */
+    private final transient List<User> admins = Collections.unmodifiableList(
         Arrays.asList(
+            // @checkstyle MultipleStringLiteralsCheck (2 lines)
             new User("admin@dp.com", "admin"),
             new User("admin@gmail.com", "admin")
         )
@@ -63,7 +67,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      */
     @PostConstruct
     public final void init() {
-        for (User admin : this.admins) {
+        for (final User admin : this.admins) {
             this.dao.save(admin);
         }
         this.dao.save(new User("a", "a1"));
@@ -74,7 +78,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public final UserDetails loadUserByUsername(final String username) {
         UserDetails result = null;
-        final String role = this.isAdmin(username) ? "ROLE_ADMIN" : "ROLE_USER";
+        String role = "ROLE_USER";
+        if (this.isAdmin(username)) {
+            role = "ROLE_ADMIN";
+        }
         final User user = this.dao.findByEmail(username);
         if (user != null) {
             result = new org.springframework.security.core.userdetails.User(
@@ -86,9 +93,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return result;
     }
 
-    private final boolean isAdmin(final String username) {
+    /**
+     * Checks if the specified user name is in the admin list.
+     * @param username The specified user name.
+     * @return Either {@code true} or {@code false}.
+     */
+    private boolean isAdmin(final String username) {
         boolean result = false;
-        for (User user : this.admins) {
+        for (final User user : this.admins) {
             if (user.getEmail().equalsIgnoreCase(username)) {
                 result = true;
                 break;
